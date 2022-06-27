1ba1a54465794d2beef4b18a1cde0ee953494180
Eli Rindler <elirindler@icloud.com>
2022-06-27 18:52:06 -0400

    Create Initial Repository

---
+++ Main.py
@@ -0,0 +1,5 @@
+import Tile
+#test
+tile1 = Tile.Tile(1)
+
+print(tile1.get_tileNum())
---
+++ Tile.py
@@ -0,0 +1,12 @@
+class Tile():
+    def __init__(self, tileNum):
+        self.tileNum = tileNum
+
+    def close_tile(self):
+        pass
+    
+    def open_tile(self):
+        pass
+
+    def get_tileNum(self):
+        return self.tileNum
