823c958283cd3c04b864b852bdc949017cb7c69f
Eli Rindler <elirindler@icloud.com>
2022-06-27 23:10:54 -0400

    Organized Func.py and simplified Main.py

--- Func.py
+++ Func.py
@@ -1,2 +1,10 @@
-class Func():
-    pass
+import Tile
+
+
+#Creates Tile Objects
+def CreateTileObjs(numTiles = 9):
+    tiles = []
+    for i in range(1, numTiles + 1):
+        i = Tile.Tile(i)
+        tiles.append(i)
+    return tiles
--- Main.py
+++ Main.py
@@ -1,15 +1,6 @@
-import Tile
 import Func
 
-#Creates Tile Objects
-def CreateTileObjs(numTiles = 9):
-    tiles = []
-    for i in range(1, numTiles + 1):
-        i = Tile.Tile(i)
-        tiles.append(i)
-    return tiles
-
-tiles = CreateTileObjs(10)
+tiles = Func.CreateTileObjs()
 
 for i in range(len(tiles)):
     print(tiles[i].get_tileNum())
