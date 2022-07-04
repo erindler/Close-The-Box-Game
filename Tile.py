7089fcf26041819af8b7195a3278131009300fa7
Eli Rindler <elirindler@icloud.com>
2022-07-03 20:03:47 -0400

    Created Tiles and Updated Tile class

--- Tile.py
+++ Tile.py
@@ -1,15 +1,21 @@
+import os
 #Tile Class
 class Tile():
     def __init__(self, tileNum, tileWidth, TILEHEIGHT):
         self.tileNum = tileNum
         self.tileWidth = tileWidth
         self.tileHeight = TILEHEIGHT
+        self.url = os.path.join("Assets", "Tile" + tileNum + ".jpg")
+        self.open = True
 
     def close_tile(self):
-        pass
+        self.open = False
     
     def open_tile(self):
-        pass
+        self.open = True
+
+    def get_tile_state(self):
+        return self.open
 
     def get_tileNum(self):
         return self.tileNum
